82f22446e384371e2ec506174ba7c33098799707
freezy <freezy@kodi.tv>
2021-03-15 16:17:44 +0100

    fix: Check for position during paint

--- lib/src/widgets/tutorial_coach_mark_widget.dart
+++ lib/src/widgets/tutorial_coach_mark_widget.dart
@@ -108,6 +108,8 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget> {
       return SizedBox.shrink();
     }
 
+    List<Widget> children = List();
+
     var positioned = Offset(
       target.offset.dx + target.size.width / 2,
       target.offset.dy + target.size.height / 2,
